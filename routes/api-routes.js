d3a2c90ef8d677204984cf3f39e16be66ac1398b
Erin <eringlabe@gmail.com>
2017-05-05 21:03:34 -0700

    api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,23 @@
+var db = require("../models");
+
+module.exports = function(app) {
+    app.get("/api/nonDevouredBurgers", function(req, res) {
+        db.Burger.findAll({
+            where: {
+                devoured: 0
+            }
+        }).then(function(dbBurger) {
+            res.json(dbBurger);
+        });
+    });
+
+    app.get("/api/devouredBurgers", function(req, res) {
+        db.Burger.findAll({
+            where: {
+                devoured: 1
+            }
+        }).then(function(dbBurger) {
+            res.json(dbBurger);
+        });
+    });
+};
